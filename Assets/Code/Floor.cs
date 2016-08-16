f1468fa28f64f990a34fbb5186338fbbd01aae9a
Alonso Parra <alienx9889@gmail.com>
2016-08-16 18:56:34 -0400

    feat(Floor.cs):Testing Hover Behaviour

--- Assets/Code/Floor.cs
+++ Assets/Code/Floor.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Floor : MonoBehaviour {
 
+	private Color hoverColor, normalColor;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		hoverColor = new Color32(111, 159, 99, 255);
+		normalColor = new Color32(255, 255, 255, 255);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnMouseOver(){
+		this.GetComponent<SpriteRenderer>().color = hoverColor;
+	}
+
+	void OnMouseExit(){
+		this.GetComponent<SpriteRenderer>().color = normalColor;
 	}
 }
