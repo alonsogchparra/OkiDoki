c39e09b00be10b55a813b29391458f252d036cad
Alonso Parra <alienx9889@gmail.com>
2016-08-16 15:32:35 -0400

    feat(Door.cs): Adding basic Mouse Fuctions

--- Assets/Code/Door.cs
+++ Assets/Code/Door.cs
@@ -3,13 +3,47 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 
+	public Sprite doorClosed, doorOpened;
+	public GameObject doorShine; 
+
+	private SpriteRenderer spriteRender;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		spriteRender = GetComponent<SpriteRenderer>();
+
+		if(spriteRender == null)
+			spriteRender.sprite = doorClosed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+
+	void ChangeSprite(){
+
+		if(spriteRender.sprite == doorClosed)
+			spriteRender.sprite = doorOpened;
+	
+	}
+
+	void OnMouseExit(){
+		doorShine.SetActive(false);
+	}
+
+	void OnMouseOver(){
+
+		if(spriteRender.sprite == doorClosed)
+			doorShine.SetActive(true);
+
+		if(Input.GetMouseButtonDown(0)){
+
+			spriteRender.sprite = doorOpened;
+
+		}
+		
 	}
 }
