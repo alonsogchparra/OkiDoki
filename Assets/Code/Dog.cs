3abcee3f9539e02ed27c040630db98f26a3dc70e
Alonso Parra <alienx9889@gmail.com>
2016-08-16 23:22:32 -0400

    feat(Dog.cs):Adding Sprites on Code to the Dog

--- Assets/Code/Dog.cs
+++ Assets/Code/Dog.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Dog : MonoBehaviour {
 
+	public Sprite dogHappy, dogKeys, dogBalloon, dogSurprise, dogEating, dogWalking, dogFindingBallon;
+
+	private SpriteRenderer spriteRender;
+
 	// Use this for initialization
 	void Start () {
 	
