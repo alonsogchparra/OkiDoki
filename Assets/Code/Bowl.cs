b2713c72a7428ce52bb0c68c9101ac8af5fde212
Alonso Parra <alienx9889@gmail.com>
2016-08-17 14:35:19 -0400

    feat(Bowl.cs): Added State Machine

--- Assets/Code/Bowl.cs
+++ Assets/Code/Bowl.cs
@@ -3,10 +3,21 @@ using System.Collections;
 
 public class Bowl : MonoBehaviour {
 
+	public enum BowlState {Empty, Food, Water, SignWater, SignFood }
+
+	public BowlState currentState = BowlState.Empty;
 	public GameObject bowlShine;
+	public Sprite bowlEmpty, bowlWater, bowlFood;
+
+	private SpriteRenderer spriteRender;
 
 	// Use this for initialization
 	void Start () {
+
+		spriteRender = GetComponent<SpriteRenderer>();
+
+		if(spriteRender == null || currentState == BowlState.Empty)
+			spriteRender.sprite = bowlEmpty;
 	
 	}
 	
