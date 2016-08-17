d62f2421a4fbfaea53197947c4972c7f051b0812
Alonso Parra <alienx9889@gmail.com>
2016-08-17 14:41:13 -0400

    feat(Dog.cs): added State Machine

--- Assets/Code/Dog.cs
+++ Assets/Code/Dog.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class Dog : MonoBehaviour {
 
-	public Sprite dogHappy, dogKeys, dogBalloon, dogSurprise, dogEating, dogWalking, dogFindingBallon;
+	public enum DogState { Happy, HasKeys, HasBalloon, Surprise, Eating, Drinking, Walking, FindingBallon }
+
+	public DogState currentState = DogState.HasKeys;
+	public Sprite dogHappy, dogHasKeys, dogHasBalloon, dogSurprise, dogEating, dogWalking, dogFindingBallon;
 
 	private SpriteRenderer spriteRender;
 
 	// Use this for initialization
 	void Start () {
+		spriteRender = GetComponent<SpriteRenderer>();
+
+		if(spriteRender == null || currentState == DogState.HasKeys)
+			spriteRender.sprite = dogHasKeys;
 	
 	}
 	
