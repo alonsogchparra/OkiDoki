3c1efb9e924bd4a74abe581177636a7b695ab489
Alonso Parra <alienx9889@gmail.com>
2016-08-18 09:33:37 -0400

    feat(Blender.cs): Added Mouse Button Down to Recharge Energy

--- Assets/Code/Blender.cs
+++ Assets/Code/Blender.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class Blender : MonoBehaviour {
 
 	public GameObject blenderShine;
+	public Juice juice;
 
 	// Use this for initialization
 	void Start () {
+
+		juice = GameObject.Find("Levels").GetComponent<Juice>();
 	
 	}
 	
@@ -17,6 +20,13 @@ public class Blender : MonoBehaviour {
 
 	void OnMouseOver(){
 		blenderShine.SetActive(true);
+
+		if(Input.GetMouseButtonDown(0) && juice.lvlNumber < 5) {
+
+			juice.lvlNumber = 5;
+
+		}
+
 	}
 
 	void OnMouseExit(){
