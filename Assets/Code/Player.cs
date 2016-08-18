954298358ebab85e20d46c7b231640e7417cd67e
Alonso Parra <alienx9889@gmail.com>
2016-08-17 23:29:30 -0400

    feat(Player.cs): Added Sprites and SpriteRender

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -6,9 +6,18 @@ public class Player : MonoBehaviour {
 	public enum PlayerState { Happy, Normal, Dizzy, Sleepy }
 
 	public PlayerState currentState = PlayerState.Happy;
+	public Sprite okiHappy, okiNormal, okiDizzy, okiSleepy;
+
+	private SpriteRenderer spriteRender;
 
 	// Use this for initialization
 	void Start () {
+
+		spriteRender = GetComponent<SpriteRenderer>();
+
+		if(spriteRender.sprite == null || currentState == PlayerState.Happy) {
+			spriteRender.sprite = okiHappy;
+		}
 	
 	}
 	
