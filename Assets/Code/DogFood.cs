f513926b0691e8daf54b4642e73163ba9138094d
Alonso Parra <alienx9889@gmail.com>
2016-08-21 17:03:47 -0400

    fea:(DogFood.cs): Added conditional to only work on floor 5 and with bowl activated, still work on that to can change the bowl sprite

--- Assets/Code/DogFood.cs
+++ Assets/Code/DogFood.cs
@@ -3,20 +3,46 @@ using System.Collections;
 
 public class DogFood : MonoBehaviour {
 
-	public GameObject dogFoodShine;
+	public GameObject dogFoodShine, bowlPlayer;
+	public bool canFeedDog;
+
+	private Player player;
 
 	// Use this for initialization
 	void Start () {
 	
+		player = GameObject.Find("Oki").GetComponent<Player>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(player.transform.position.x == 0.36f) {
+			canFeedDog = true;
+		} else {
+			canFeedDog = false;
+		}
 	
 	}
 
 	void OnMouseOver(){
 		dogFoodShine.SetActive(true);
+
+		//TODO: ARREGLAR EL SPRITE BOWL DEL PLAYER PARA QUE CAMBIE
+
+//		if(Input.GetMouseButtonDown(0) && canFeedDog 
+//			&& bowlPlayer.activeSelf && player.bowlSpriteRender.sprite == player.bowlEmpty) {
+//
+//			player.bowlSpriteRender.sprite = player.bowlFood;
+//		}
+
+		if(Input.GetMouseButtonDown(0) && canFeedDog){
+			player.bowlSpriteRender.sprite = player.bowlFood;
+		}
+
+//		print(canFeedDog);
+
 	}
 
 	void OnMouseExit(){
