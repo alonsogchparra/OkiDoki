3d602ee6590ceba7550f02a4724fb426d646dc43
Alonso Parra <alienx9889@gmail.com>
2016-08-27 16:20:13 -0400

    feat(Keys.cs): Now The keys are able to be picked by the player

--- Assets/Code/Keys.cs
+++ Assets/Code/Keys.cs
@@ -4,19 +4,69 @@ using System.Collections;
 public class Keys : MonoBehaviour {
 
 	public GameObject keysShine;
+	public Sprite sprite;
+	public bool canTakeIt;
 
+	private SpriteRenderer spriteRender;
+	private Color alphaFullColor, alphaZeroColor;
+	private Dog dog;
+	private Player player;
 	// Use this for initialization
 	void Start () {
-	
+
+		dog = GameObject.Find("Doki").GetComponent<Dog>();
+		player = GameObject.Find("Oki").GetComponent<Player>();
+
+		spriteRender = GetComponent<SpriteRenderer>();
+
+		if(spriteRender == null) {
+			spriteRender.sprite = sprite;
+		}
+
+		alphaZeroColor = spriteRender.color;
+		alphaZeroColor.a = 0f;
+		alphaFullColor = spriteRender.color;
+		alphaFullColor.a = 1f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (dog.transform.position.x == transform.position.x) {
+			
+			spriteRender.color = alphaZeroColor;
+			GetComponent<BoxCollider2D>().enabled = false;
+			keysShine.SetActive(false);
+
+		} else if(dog.transform.position.x != transform.position.x) {
+
+			spriteRender.color = alphaFullColor;
+			GetComponent<BoxCollider2D>().enabled = true;
+
+		}
+
+		if(player.transform.position.x == -4.47f && spriteRender.color == alphaFullColor) {
+			canTakeIt = true;
+		}
+
+		if(player.playerKeys.activeSelf) {
+			spriteRender.color = alphaZeroColor;
+		}
 	
 	}
 
 	void OnMouseOver(){
-		keysShine.SetActive(true);
+		
+		if(spriteRender.color == alphaFullColor) {
+			keysShine.SetActive(true);
+		}
+
+
+		if(Input.GetMouseButtonDown(0) && canTakeIt) {
+			player.playerKeys.SetActive(true);
+			spriteRender.color = alphaZeroColor;
+		}
 	}
 
 	void OnMouseExit(){
