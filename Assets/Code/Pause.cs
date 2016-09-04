d29f2634a818e3de378cf19e612f1daab929af5a
Alonso Parra <alienx9889@gmail.com>
2016-09-04 00:45:34 -0400

    feat(Pause.cs): New Script, New Function to activate Pause Panel

---
+++ Assets/Code/Pause.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject pausePanel;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PausePanelActivated() {
+
+		pausePanel.SetActive(true);
+		
+	}
+
+	public void PausePanelDeactivated() {
+
+		pausePanel.SetActive(false);
+
+	}
+
+}
