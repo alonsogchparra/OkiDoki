41f0851eb2520d5e0c5859f92c621815ea5b14f6
aliens9889 <alienx9889@gmail.com>
2016-11-01 11:54:51 -0400

    feat(Pause.cs): When the Pause Panel is activated, the script will disable the colliders with Tags Floor & Touchable.

--- Assets/Code/Pause.cs
+++ Assets/Code/Pause.cs
@@ -14,13 +14,35 @@ public class Pause : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(!pausePanel.activeSelf) {
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Floor")) {
+				g.GetComponent<PolygonCollider2D>().enabled = true;
+			}
+			
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Touchable")) {
+				g.GetComponent<BoxCollider2D>().enabled = true;
+			}
+
+		} else {
+			
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Floor")) {
+				g.GetComponent<PolygonCollider2D>().enabled = false;
+			}
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Touchable")) {
+				g.GetComponent<BoxCollider2D>().enabled = false;
+
+			}
+		}
 	
 	}
 
 	public void PausePanelActivated() {
 
 		pausePanel.SetActive(true);
-		
+
 	}
 
 	public void PausePanelDeactivated() {
