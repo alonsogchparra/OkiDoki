7c85d8d539001a5fbce5fb778658461ee450ced7
aliens9889 <alienx9889@gmail.com>
2016-11-01 11:59:02 -0400

    feat(PanelControl.cs): This Script will disable the colliders with tag Floor & Touchable when GameOver Panel or Winner Panel is activated

---
+++ Assets/Code/PanelControl.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelControl : MonoBehaviour {
+
+	public GameObject winnerPanel, gameOverPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(winnerPanel.activeSelf || gameOverPanel.activeSelf) {
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Floor")) {
+				g.GetComponent<PolygonCollider2D>().enabled = false;
+			}
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Touchable")) {
+				g.GetComponent<BoxCollider2D>().enabled = false;
+
+			}
+			
+		} else if (!winnerPanel.activeSelf || !gameOverPanel.activeSelf ) {
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Floor")) {
+				g.GetComponent<PolygonCollider2D>().enabled = true;
+			}
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Touchable")) {
+				g.GetComponent<BoxCollider2D>().enabled = true;
+
+			}
+
+		}
+
+//		if(!frigdePanel.activeSelf) {
+//
+//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Floor")) {
+//				g.GetComponent<PolygonCollider2D>().enabled = true;
+//			}
+//
+//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Touchable")) {
+//				g.GetComponent<BoxCollider2D>().enabled = true;
+//			}
+//
+//		} else {
+//
+//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Floor")) {
+//				g.GetComponent<PolygonCollider2D>().enabled = false;
+//			}
+//
+//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Touchable")) {
+//				g.GetComponent<BoxCollider2D>().enabled = false;
+//
+//			}
+//		}
+
+	}
+}
