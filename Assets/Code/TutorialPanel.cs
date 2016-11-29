3ffeac2fb5d301bad1ec82fafd33927e40f74540
aliens9889 <alienx9889@gmail.com>
2016-11-28 23:30:08 -0400

    feat(TutorialPanel): New code to try to deactivate the colliders (Still in progress)

---
+++ Assets/Code/TutorialPanel.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialPanel : MonoBehaviour {
+
+	public GameObject tutorialPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(tutorialPanel.activeSelf) {
+
+			print("Desactivo todos los colliders");
+
+						foreach(GameObject g in GameObject.FindGameObjectsWithTag("Floor")) {
+							g.GetComponent<PolygonCollider2D>().enabled = false;
+						}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Touchable")) {
+			//				g.GetComponent<BoxCollider2D>().enabled = false;
+			//
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Keys")) {
+			//				g.GetComponent<BoxCollider2D>().enabled = false;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Bowl")) {
+			//				g.GetComponent<BoxCollider2D>().enabled = false;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("BalloonSeven")) {
+			//				g.GetComponent<CircleCollider2D>().enabled = false;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("BalloonFive")) {
+			//				g.GetComponent<CircleCollider2D>().enabled = false;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("BalloonTwo")) {
+			//				g.GetComponent<CircleCollider2D>().enabled = false;
+			//			}
+
+
+
+		} else {
+
+			print("Activo todos los colliders");
+
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Floor")) {
+			//				g.GetComponent<PolygonCollider2D>().enabled = true;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Touchable")) {
+			//				g.GetComponent<BoxCollider2D>().enabled = true;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Keys")) {
+			//				g.GetComponent<BoxCollider2D>().enabled = true;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Bowl")) {
+			//				g.GetComponent<BoxCollider2D>().enabled = true;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("BalloonSeven")) {
+			//				g.GetComponent<CircleCollider2D>().enabled = true;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("BalloonFive")) {
+			//				g.GetComponent<CircleCollider2D>().enabled = true;
+			//			}
+			//
+			//			foreach(GameObject g in GameObject.FindGameObjectsWithTag("BalloonTwo")) {
+			//				g.GetComponent<CircleCollider2D>().enabled = true;
+			//			}
+
+		}
+	
+	}
+}
