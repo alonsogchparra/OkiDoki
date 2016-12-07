4f81fc4be9dd8391d028fb9843b98644cfebe8bc
aliens9889 <alienx9889@gmail.com>
2016-12-07 15:11:52 -0400

    feat(Project): Admob Plugin added

---
+++ Assets/Code/AdManager.cs
@@ -0,0 +1,43 @@
+﻿using admob;
+using UnityEngine;
+
+public class AdManager : MonoBehaviour {
+
+	public static AdManager Instance { set; get; }
+
+	private static readonly string TOP_BANNER_ID = "";
+	private static readonly string VIDEO_ID = "ca-app-pub-7267736941458952/1180908028";
+
+	void Start () {
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+
+		#if UNITY_EDITOR
+		Debug.Log("Unable to play ads from editor");
+		#elif UNITY_ANDROID        
+		Admob.Instance().initAdmob(TOP_BANNER_ID, VIDEO_ID);
+		Admob.Instance().loadInterstitial();
+		#endif
+	}
+
+	public void ShowTopBanner()
+	{        
+		#if UNITY_EDITOR
+		Debug.Log("Unable to play ads from editor");
+		#elif UNITY_ANDROID
+		Admob.Instance().showBannerRelative(AdSize.Banner, AdPosition.TOP_CENTER, 5);        
+		#endif
+	}
+
+	public void ShowVideo()
+	{
+		#if UNITY_EDITOR
+		Debug.Log("Unable to play ads from editor22");
+		#elif UNITY_ANDROID
+		if (Admob.Instance().isInterstitialReady())
+		{
+		Admob.Instance().showInterstitial();
+		}
+		#endif
+	}
+}
