c4804caf9f17562dfb052b5a67f14d2fbea3b750
aliens9889 <alienx9889@gmail.com>
2017-04-02 01:52:18 -0400

    Feat(LivinRoom): new code according new level - level 2(LivinRoom)

---
+++ Assets/Code/LivinRoom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivinRoom : MonoBehaviour {
+
+	public GameObject tutorialPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (tutorialPanel.activeSelf ) {
+			return;
+		} 
+		else if(!tutorialPanel.activeSelf ) {
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Floor")) {
+				g.GetComponent<PolygonCollider2D>().enabled = true;
+			}
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("Touchable")) {
+				g.GetComponent<BoxCollider2D>().enabled = true;
+			}
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("DogToy")) {
+				g.GetComponent<BoxCollider2D>().enabled = true;
+			}
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("CouchOne")) {
+				g.GetComponent<BoxCollider2D>().enabled = true;
+			}
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("CouchTwo")) {
+				g.GetComponent<BoxCollider2D>().enabled = true;
+			}
+
+			foreach(GameObject g in GameObject.FindGameObjectsWithTag("CouchThree")) {
+				g.GetComponent<BoxCollider2D>().enabled = true;
+			}
+
+		}
+		
+	}
+}
